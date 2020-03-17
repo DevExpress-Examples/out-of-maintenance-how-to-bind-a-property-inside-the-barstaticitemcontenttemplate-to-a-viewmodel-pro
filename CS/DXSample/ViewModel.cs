121fcec44ee21a1e1d3c7053b91839d7acdd17bf
Michael Ch (DevExpress) <45622253+MichaelDX@users.noreply.github.com>
2020-03-17 18:02:37 +0300

    update

---
+++ CS/DXSample/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Threading;
+using DevExpress.Mvvm;
+
+namespace DXSample {
+    public class ViewModel : ViewModelBase {        
+        DispatcherTimer Timer;
+        public DateTime CurrentValue {
+            get { return GetValue<DateTime>(); }
+            set { SetValue(value); }
+        }
+        public string Content {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+        public ViewModel() {
+            Content = "Hello, World!";
+            Timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e) {
+            CurrentValue = DateTime.Now;
+        }
+        ~ViewModel() {
+            Timer.Stop();
+            Timer.Tick -= Timer_Tick;
+            Timer = null;
+        }
+    }
+}
